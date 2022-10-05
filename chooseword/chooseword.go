3e3da5ce0a1193538a154ca4a535bb5124fffbc3
kjulie <julie.kreuwen@ynov.com>
2022-10-05 14:22:44 +0100

    fonction qui choisi au hazard un mot dans le fichier txt

---
+++ chooseword/chooseword.go
@@ -0,0 +1,30 @@
+package chooseword
+
+import (
+	"bufio"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func ChooseWord() string {
+	var words []string
+	rand.Seed(time.Now().UTC().UnixNano())
+	mots, err := os.Open("Mots.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(mots)
+
+	nbwords := 1
+
+	for scanner.Scan() {
+		nbwords++
+		words = append(words, scanner.Text())
+	}
+	indexword := rand.Intn(nbwords)
+
+	return words[indexword]
+}
