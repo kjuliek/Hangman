034be7e0f281e5f35bddb13f37da218d73ea53ee
White_ONyme <danny.laplace@ynov.com>
2022-10-05 16:47:48 +0200

    func deja afficher

--- shangman/pendu.go
+++ shangman/pendu.go
@@ -5,33 +5,38 @@ import (
 	"hangman/printhangman"
 )
 
-func Deja_Affichee() {
-
+func Deja_Affichee(Hangman *Hangman, lettre string) bool {
+	for i := 0; i < len(Hangman.Mot_Afficher); i++ {
+		if string(Hangman.Mot_Afficher[i]) == lettre {
+			return true
+		}
+	}
+	return false
 }
 
-func Pendu(hangman *Hangman) {
+func Pendu(Hangman *Hangman) {
 	var lettre string
 	lettredanslemot := false
 
 	fmt.Print("choisissez une lettre : ")
 	fmt.Scan(&lettre)
-	for i := 0; i < len(hangman.Mot_de_Depart); i++ {
-		if lettre == string(hangman.Mot_de_Depart[i]) {
+	for i := 0; i < len(Hangman.Mot_de_Depart); i++ {
+		if lettre == string(Hangman.Mot_de_Depart[i]) {
 			lettredanslemot = true
-			if Deja_Affichee() {
-				hangman.Attempts--
+			if Deja_Affichee(Hangman, lettre) {
+				Hangman.Attempts--
 				fmt.Println("Oh non ! La lettre a déjà été affiché, il te reste ", hangman.Attempts, " essais.")
-				printhangman.PrintHangman(hangman.Attempts)
+				printhangman.PrintHangman(Hangman.Attempts)
 			} else {
-				printhangman.PrintHangman(hangman.Attempts)
+				printhangman.PrintHangman(Hangman.Attempts)
 			}
 		}
 
 	}
 	if !lettredanslemot {
-		hangman.Attempts--
-		fmt.Println("Il te reste ", hangman.Attempts, " essais.")
-		printhangman.PrintHangman(hangman.Attempts)
+		Hangman.Attempts--
+		fmt.Println("Il te reste ", Hangman.Attempts, " essais.")
+		printhangman.PrintHangman(Hangman.Attempts)
 	}
 
 }
