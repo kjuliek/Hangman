a295e96cc6529d2174ce1b10e7c02f1b16eb04f2
patchouf <adriana.pullig@ynov.com>
2022-10-24 15:19:40 +0200

    corriger

--- revealLetters/revealLettersStart.go
+++ revealLetters/revealLettersStart.go
@@ -5,40 +5,40 @@ import (
 	"time"
 )
 
-func ReveallettersStart(mot string) string {
-	new_mot := ""
-	Nb_Lettre_To_Reveal := (len(mot) / 2) - 1
-	Position_deja_reveal := []int{}
+func ReveallettersStart(word string) string {
+	new_word := ""
+	Nb_Letter_To_Reveal := (len(word) / 2) - 1
+	Position_reveal := []int{}
 	rand.Seed(time.Now().UTC().UnixNano())
 	Verification := true
 
-	for i := 0; i < Nb_Lettre_To_Reveal; i++ {
-		Pose_Tmp := rand.Intn(len(mot))
+	for i := 0; i < Nb_Letter_To_Reveal; i++ {
+		Pose_Time := rand.Intn(len(word))
 		Verification = true
-		for j := 0; j < len(Position_deja_reveal); j++ {
-			if Pose_Tmp == Position_deja_reveal[j] {
+		for j := 0; j < len(Position_reveal); j++ {
+			if Pose_Time == Position_reveal[j] {
 				Verification = false
 				i--
 			}
 		}
 		if Verification {
-			Position_deja_reveal = append(Position_deja_reveal, Pose_Tmp)
+			Position_reveal = append(Position_reveal, Pose_Time)
 		}
 	}
 
-	for i := 0; i < len(mot); i++ {
+	for i := 0; i < len(word); i++ {
 		Verification = true
-		for j := 0; j < len(Position_deja_reveal); j++ {
-			if mot[i] == mot[Position_deja_reveal[j]] {
-				new_mot = new_mot + string(mot[i])
+		for j := 0; j < len(Position_reveal); j++ {
+			if word[i] == word[Position_reveal[j]] {
+				new_word = new_word + string(word[i])
 				Verification = false
 				break
 			}
 		}
 		if Verification {
-			new_mot = new_mot + "_"
+			new_word = new_word + "_"
 		}
 	}
 
-	return new_mot
+	return new_word
 }
