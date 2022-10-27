077b4ef62535a1739d43de6795fb47b04c5519dc
patchouf <adriana.pullig@ynov.com>
2022-10-27 14:21:21 +0200

    commentaire

--- revealLetters/revealLettersStart.go
+++ revealLetters/revealLettersStart.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-func ReveallettersStart(word string) string {
+func ReveallettersStart(word string) string { //affichage des lettres
 	new_word := ""
-	Nb_Letter_To_Reveal := (len(word) / 2) - 1
-	Position_reveal := []int{}
+	Nb_Letter_To_Reveal := (len(word) / 2) - 1 //nombre de lettres révélé, longueur du mot divisé par 2 moins 1
+	Position_reveal := []int{}                 //position de la lettre révélé
 	rand.Seed(time.Now().UTC().UnixNano())
 	Verification := true
 
-	for i := 0; i < Nb_Letter_To_Reveal; i++ {
+	for i := 0; i < Nb_Letter_To_Reveal; i++ { //incrémentation de la lettre dans le mot
 		Pose_Time := rand.Intn(len(word))
 		Verification = true
 		for j := 0; j < len(Position_reveal); j++ {
@@ -26,7 +26,7 @@ func ReveallettersStart(word string) string {
 		}
 	}
 
-	for i := 0; i < len(word); i++ {
+	for i := 0; i < len(word); i++ { //prends le mot et ajoute les tirets
 		Verification = true
 		for j := 0; j < len(Position_reveal); j++ {
 			if word[i] == word[Position_reveal[j]] {
