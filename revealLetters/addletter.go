dcf5bfff72d95479878025b60e24f8832db9e162
patchouf <adriana.pullig@ynov.com>
2022-10-27 14:22:40 +0200

    commentaire

--- revealLetters/addletter.go
+++ revealLetters/addletter.go
@@ -1,6 +1,6 @@
 package revealLetters
 
-func AddLetter(letter, shown_word, starting_word string) string {
+func AddLetter(letter, shown_word, starting_word string) string { //ajout des lettres du mots affiché à trou
 	var new_word string
 
 	for index, l := range shown_word {
