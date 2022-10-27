5ecf63c9169144adda39d84e946c67d607cd9118
patchouf <adriana.pullig@ynov.com>
2022-10-27 16:12:41 +0200

    anglais

--- printword/printword.go
+++ printword/printword.go
@@ -5,14 +5,14 @@ import (
 	"hangman/asciiart"
 )
 
-func PrintWord(word string) { //choix du mot et affichage de celui-ci
+func PrintWord(word string) { //choice of the word and display of it
 	for i := 0; i < len(word); i++ {
 		fmt.Print(string(word[i]), " ")
 	}
 	fmt.Println()
 }
 
-func PrintAsciArt(starting_word, shown_word string, letter_ascii_art [][]string) { //incrémentation de asciiart dans l'affichage du mot
+func PrintAsciArt(starting_word, shown_word string, letter_ascii_art [][]string) { //incrementation of the asciiart when the word is showed
 	sort_word := "_" + asciiart.SortWord(starting_word)
 	for k := 0; k < 12; k++ {
 		for _, letter := range shown_word {
